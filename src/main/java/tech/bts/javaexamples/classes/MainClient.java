c872c6cf3e075468688ff52d2ecaa9a0ab83d483
fmaylinch <ferranmaylinch@gmail.com>
2018-10-05 12:27:19 +0200

    Create Client (model) class

---
+++ src/main/java/tech/bts/javaexamples/classes/MainClient.java
@@ -0,0 +1,12 @@
+package tech.bts.javaexamples.classes;
+
+public class MainClient {
+
+    public static void main(String[] args) {
+
+        Client c1 = new Client();
+
+        c1.setName("some one");
+        c1.setEmail("some@gmail.com");
+    }
+}
