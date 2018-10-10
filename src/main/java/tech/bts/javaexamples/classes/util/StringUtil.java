0a36c8620e2886cf45c106237652b35595a277ac
fmaylinch <ferranmaylinch@gmail.com>
2018-10-10 12:26:30 +0200

    Clean and document StringUtil

--- src/main/java/tech/bts/javaexamples/classes/util/StringUtil.java
+++ src/main/java/tech/bts/javaexamples/classes/util/StringUtil.java
@@ -1,11 +1,12 @@
 package tech.bts.javaexamples.classes.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class StringUtil {
 
-    static String repeatString (String s, int n) {
+    /**
+     * Returns a string made of `s` repeated `n` times.
+     * For example: repeat("go", 3) returns "gogogo".
+     */
+    public static String repeat (String s, int n) {
 
         String result = "";
         for (int i = 1; i <= n; i++) {
@@ -14,22 +15,16 @@ public class StringUtil {
         return result;
     }
 
-    static String padLeft (String s, int n) {
+    /** Returns a string containing `s` plus spaces to the left so the resulting string has length `n` */
+    public static String padLeft (String s, int n) {
 
-        return repeatString(" ", n - s.length()) + s;
+        return repeat(" ", n - s.length()) + s;
     }
 
-    static String padRight (String s, int n) {
+    /** Returns a string containing `s` plus spaces to the right so the resulting string has length `n` */
+    public static String padRight (String s, int n) {
 
-        return s + repeatString(" ", n - s.length());
+        return s + repeat(" ", n - s.length());
     }
 
-    static List<Integer> listOfNumbers (int start, int end) {
-
-        List<Integer> result = new ArrayList<>();
-        for (int i = start; i <= end; i++) {
-            result.add(i);
-        }
-        return result;
-    }
 }
